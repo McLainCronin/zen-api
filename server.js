5e03af049db58465d5d6936ecb6757ec567195a8
McLainCronin <cronin97@gmail.com>
2024-11-06 14:29:00 -0800

    Add success and cancel HTML pages, update .gitignore, and implement checkout session route

--- server.js
+++ server.js
@@ -1,10 +1,67 @@
 const express = require('express')
+const { generateApiKey } = require('generate-api-key')
 const app = express()
 const PORT = 1337
+require('dotenv').config()
+
+//Variables
+const STRIPE_SECRET_KEY = process.env.STRIPE_SK
+const stripe = require('stripe')(STRIPE_SECRET_KEY)
+const DOMAIN = 'http://localhost:1337'
 
 //middleware
 app.use(express.static('public'))
 
 //routes
+app.post('create-checkout-session/:product', async (req, res) => {
+    const { product } = req.params
+    let mode, price_ID, line_items
+
+    if (product === 'sub') {
+        price_ID = 'price_1QIHANBVYU2aQnjNeNUix3uy'
+        mode = 'subscription'
+        line_items = [
+            {
+                price: price_ID,
+            }
+        ]
+    } else if (product === 'pre') {
+        price_ID = 'price_1QIH2pBVYU2aQnjNgAxwYJcM'
+        mode = 'payment'
+        line_items = [
+            {
+                price: price_ID,
+                qualtity: 1
+            }
+        ]
+    } else {
+        return res.sendStatus(403)
+    }
+
+    const newAPIKey = generateApiKey()
+    const customer = await stripe.constomers.create({
+        metadata: {
+            APIkey: newAPIKey,
+        }
+    })
+
+    const stripeCustomerId = customer.id
+    const session = await stripe.checkout.sessions.create({
+        customer: stripeCustomerId,
+        metadata: {
+            APIkey: newAPIKey,
+            payment_type: product
+        },
+        line_items,
+        mode: mode,
+        success_url: `${DOMAIN}/success.html?api_key=${newAPIKey}`,
+        cancel_url: `${DOMAIN}/cancel.html`,
+    })
+
+    //create firebase record
+
+    //use webhook to access the firebase entry for that api key and ensure that billing infor is updated accordingly
+    res.redirect(303, session.url)
+})
 
 app.listen(PORT, () => console.log(`Server has started on port: ${PORT}`))
