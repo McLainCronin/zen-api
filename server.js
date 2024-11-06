76e84c9bf86b660f92bef504165d09be011940dc
McLainCronin <cronin97@gmail.com>
2024-11-06 13:31:05 -0800

    Add middleware for serving static files and organize server initialization. Created home page

--- server.js
+++ server.js
@@ -2,4 +2,9 @@ const express = require('express')
 const app = express()
 const PORT = 1337
 
-app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+//middleware
+app.use(express.static('public'))
+
+//routes
+
+app.listen(PORT, () => console.log(`Server has started on port: ${PORT}`))
